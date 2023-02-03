954621a7f9558ad7d0a9006fe6a779b87366b4a4
cemrekaraoglan <cemre.karaoglan@rwth-aachen.de>
2023-02-03 22:33:41 +0100

    Initial commit

---
+++ script/pkg_launch.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+cd ~/ros2
+
+. install/setup.bash
+
+ros2 launch slicer visual_launch.py
---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+import glob
+import os
+
+package_name = 'slicer'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'robot_model/meshes'), glob.glob('robot_model/meshes/*.dae')),
+        (os.path.join('share', package_name, 'robot_model'), glob.glob('robot_model/robot_description2.urdf')),
+        (os.path.join('share', package_name, 'launch'), glob.glob('launch/visual_launch.py')),
+        (os.path.join('share', package_name, 'rviz'), glob.glob('config/rvitz_config.rviz')),
+        (os.path.join('share', package_name, 'config'), glob.glob('config/param.yaml')) #place the file in the launch folder in this path and get it from this path
+
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='cemrekaraoglan',
+    maintainer_email='cemre.karaoglan@rwth-aachen.de',
+    description='A robot',
+    license='RWTH Aachen',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': ['slicer_cutter = slicer.main3:main', 'slicer_visualizer = slicer.main2:main', 'slicer = slicer.main:main'
+   
+        ],
+    },
+)
