8fb4eb86ab83d81408e4038f5d292f8b53ac3b9e
cemrekaraoglan <cemre.karaoglan@rwth-aachen.de>
2023-02-04 10:22:50 +0100

    Update

--- slicer/main2.py
+++ slicer/main2.py
@@ -12,8 +12,8 @@ class Gypsum(Node):
         super().__init__(name)
 
         self.gypsum_pub = self.create_publisher(MarkerArray, 'visualization_marker_array', 10)
-        self.sub = self.create_subscription(String, "/fabrication_status/message", self.subscriber_callback, 10)
-        self.sub_msg = self.create_subscription(Float32, '/fabrication_status/dimension', 10)
+        self.sub_msg = self.create_subscription(String, "/fabrication_status/message", self.message_callback, 10)
+        self.sub_dim = self.create_subscription(Float32, '/fabrication_status/dimension', self.dimension_callback, 10)
         
         self.array = MarkerArray()
         self.gypsum = Marker()
@@ -45,29 +45,35 @@ class Gypsum(Node):
         self.gypsum.color.g = random.uniform(0.0, 0.5)
         self.gypsum.color.b = 1.0 - (self.gypsum.color.r + self.gypsum.color.g)
         
-        self.message = String()
-                    
-    def subscriber_callback(self, msg):
+        self.msg_str = String()
+        self.msg_dim = Float32()
         
-        self.message = msg.data
-        self.new_dim = 0.8
+    
+    def dimension_callback(self,msg):
+ 
+        self.msg_dim = float(msg.data)
         
-        if self.message == 'The cutting has started...':
-            self.get_logger().info(f'I am alive {self.gypsum}')
+    def message_callback(self, msg):
+        
+        self.msg_str = msg.data
+
+        if self.msg_str == 'The cutting has started':
+            self.get_logger().info(f'Hello1')
             self.gypsum.action = Marker().ADD
             self.array.markers.append(self.gypsum)
             self.gypsum_pub.publish(self.array)
             
         
-        if self.message == 'Gypsumboard is cut':
-            self.gypsum.action = Marker().REMOVE
-        
+        elif self.msg_str== 'Gypsumboard is cut':
+            self.gypsum.action = Marker().DELETE
+            
+            self.get_logger().info(f'Hello2')
             #piece 1
             self.gypsum.color.r = random.uniform(0.0, 0.5)
             self.gypsum.color.g = random.uniform(0.0, 0.5)
             self.gypsum.color.b = 1.0 - (self.gypsum.color.r + self.gypsum.color.g)
     
-            self.gypsum.scale.x = self.new_dim
+            self.gypsum.scale.x = self.msg_dim
 
             self.gypsum.action = Marker().ADD
         
@@ -79,16 +85,18 @@ class Gypsum(Node):
             self.gypsum.color.g = random.uniform(0.0, 0.5)
             self.gypsum.color.b = 1.0 - (self.gypsum.color.r + self.gypsum.color.g)
 
-            self.gypsum.scale.x = self.dim_x-float(self.new_dim)
+            self.gypsum.scale.x = self.dim_x-float(self.msg_dim)
           
 
-            self.gypsum.pose.position.x = (self.new_dim)
+            self.gypsum.pose.position.x = (self.msg_dim)
 
             self.gypsum.action = Marker().ADD
         
             self.array.markers.append(self.gypsum)
 
             self.gypsum_pub.publish(self.array)
+            
+            self.get_logger().info(f'Hello3')
 
 
 def main(args=None):
